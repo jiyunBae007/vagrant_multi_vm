23901b0de63e5bbc40fbcf741e95ea6c34642ce5
jiyunBae007 <63540652+jiyunBae007@users.noreply.github.com>
2022-05-26 00:24:31 +0900

    Update show.php

--- Vagrant_Multi/show.php
+++ Vagrant_Multi/show.php
@@ -3,10 +3,11 @@
 
 $username = 'root';
 $password = 'rootpass';
-$dsn = 'mysql:host=192.168.3.6;dbname=formresponses';
+$dsnA = 'mysql:host=192.168.3.6;dbname=formresponses';
+$dsnB = 'mysql:host=192.168.3.7;dbname=formresponses';
 
 try{
-	$db = new PDO($dsn, $username, $password);
+	$db = new PDO($dsnA, $username, $password);
 	$result=FALSE;
 
     $query = "SELECT * FROM response";
@@ -33,8 +34,33 @@ try{
         //printf("{$row['firstname']} {$row['secondname']} {$row['email']}\n");
     }
 
-} catch(PDOException $ex) {
-	echo $ex->getMessage();
+#database1이 고장나더라도 database2 server를 기반으로 서비스 운영을 가능하게 함
+} catch(PDOException $ex) {  
+	$db = new PDO($dsnB, $username, $password);
+	$result=FALSE;
+
+    $query = "SELECT * FROM response";
+
+    $stmt = $db->query($query);
+    
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    
+    echo "<table border='1'>
+    <tr>
+    <th>Firstname</th>
+    <th>Lastname</th>
+    <th>email</th>
+    </tr>";
+
+    foreach($rows as $row) {
+        echo "<tr>";
+        echo "<td>".$row['firstname']."</td>";
+        echo "<td>".$row['lastname']."</td>";
+        echo "<td>".$row['email']."</td>";
+        echo "</tr>";
+
+        //printf("{$row['firstname']} {$row['secondname']} {$row['email']}\n");
 }
 
 echo "</table>";
